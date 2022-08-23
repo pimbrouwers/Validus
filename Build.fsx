b87ddcdde70a7fb590e4697834c233b60f3ac6f7
Pim Brouwers <pbrouwers@nhlpa.com>
2022-08-23 16:18:48 -0400

    scripts update

--- Build.fsx
+++ Build.fsx
@@ -17,11 +17,14 @@ module Log =
     let info fmt = log "Info" fmt
 
 module Command =
+    let runCommand (exe : string) (args : string) =
+        Log.info "  %s %s" exe args
+        let p = Process.Start(exe, args)
+        p.WaitForExit()
+
     let runDotnet (assemblyPath : string) (tool : string) (args : string) =
         let args' = sprintf "%s \"%s\" %s" tool assemblyPath args
-        Log.info "  dotnet %s" args'
-        let p = Process.Start("dotnet", args')
-        p.WaitForExit()
+        runCommand "dotnet" args'
 
 let assemblyName = "Validus"
 let assemblyPath = Path.Join(__SOURCE_DIRECTORY__, "src", assemblyName)
